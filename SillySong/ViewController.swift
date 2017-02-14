f4a89bf315782d52c292e7418a587176de8915c5
Boxuan Zhang <zhangboxuan2008@hotmail.com>
2017-02-13 20:03:28 -0800

    Initial Commit

---
+++ SillySong/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SillySong
+//
+//  Created by Boxuan Zhang on 2/13/17.
+//  Copyright © 2017 Boxuan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
